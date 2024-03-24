4f0d9e8e03fef00f1fc7fc6d105192b41f23a026
Heical Chandra <haical31@gmail.com>
2024-03-24 11:00:51 +0700

    update 1

---
+++ KosKita/KosKita/KosKitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KosKitaApp.swift
+//  KosKita
+//
+//  Created by Heical Chandra on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct KosKitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
